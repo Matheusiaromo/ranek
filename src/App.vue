336d2bc6a9a6393e9f082d6bc788367f60e842e3
Matheus Morais <matheusiaromo@gmail.com>
2021-05-31 01:42:50 -0300

    adicionados estilos principais

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,88 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-    </div>
-    <router-view/>
+    <TheHeader />
+    <main id="main">
+      <router-view/>
+    </main>
+    <TheFooter />
   </div>
 </template>
 
+<script>
+
+import TheHeader from '@/components/TheHeader'
+import TheFooter from '@/components/TheFooter'
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
+
 <style>
 
+:root {
+  --roxo: #87f;
+  --azul: #65d;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body, ul, li, h1, h2, p {
+  margin: 0px;
+  padding: 0px;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: "Arial", 'helvetica';
+  color: #345;
+  background: url("./assets/pattern.svg") repeat top;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: var(--roxo);
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0px 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Arial";
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: var(--azul);
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main {
+  flex: 1;
+}
 </style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <p>Ranek, Alguns direitos reservados</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter'
+}
+</script>
+
+<style>
+
+footer {
+  background: var(--roxo);
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1.2rem;
+  color: #fff;
+  position: relative;
+  top: 75px;
+}
+
+</style>
